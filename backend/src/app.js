ae90b596dfa7c831006af1278a56fdaad6f42345
Srutidev Barman <srutidevbarman2002@gmail.com>
2026-05-18 16:39:04 +0530

    added helmet js security

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,8 +5,58 @@ import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 import productRouter from "./routes/product.routes.js";
 import cartRouter from "./routes/cart.routes.js";
+import helmet from "helmet";
+
 const app = express();
 
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:", "https:"],
+        connectSrc: ["'self'", "http://localhost:5173"],
+        fontSrc: ["'self'", "https:", "data:"],
+        objectSrc: ["'none'"],
+        upgradeInsecureRequests: [],
+      },
+    },
+
+    frameguard: {
+      action: "deny",
+    },
+
+    noSniff: true,
+
+    hsts: {
+      maxAge: 31536000,
+      includeSubDomains: true,
+      preload: true,
+    },
+
+    referrerPolicy: {
+      policy: "no-referrer",
+    },
+
+    crossOriginOpenerPolicy: {
+      policy: "same-origin",
+    },
+
+    crossOriginResourcePolicy: {
+      policy: "same-origin",
+    },
+
+    crossOriginEmbedderPolicy: false,
+
+    permittedCrossDomainPolicies: {
+      permittedPolicies: "none",
+    },
+
+    hidePoweredBy: true,
+  }),
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
